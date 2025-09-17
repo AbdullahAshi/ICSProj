b600a6d3033654dfe4d124180df444e8ee1c92cb
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2025-09-17 16:41:54 +0300

    -create initial project

---
+++ ICSProj/ICSProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICSProj
+//
+//  Created by Abdullah Alashi on 17/9/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
