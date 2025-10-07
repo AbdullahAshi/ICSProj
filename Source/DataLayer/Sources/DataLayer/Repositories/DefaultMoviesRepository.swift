04e7d53de86f147ab7251df652337ad58305d6ff
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2025-10-08 00:39:44 +0300

    - disable getting and storing from Cache as it is not available in DataLayer

--- Source/DataLayer/Sources/DataLayer/Repositories/DefaultMoviesRepository.swift
+++ Source/DataLayer/Sources/DataLayer/Repositories/DefaultMoviesRepository.swift
@@ -42,27 +42,27 @@ public final class DefaultMoviesRepository: DomainLayer.MoviesRepository {
         let requestDTO = MoviesRequestDTO(query: query.query, page: page)
         let task = RepositoryTask()
 
-        cache.getResponse(for: requestDTO) { [weak self, backgroundQueue] result in
-
-            if case let .success(responseDTO?) = result {
-                cached(responseDTO.toDomain())
-            }
-            guard !task.isCancelled else { return }
+//        cache.getResponse(for: requestDTO) { [weak self, backgroundQueue] result in
+//
+//            if case let .success(responseDTO?) = result {
+//                cached(responseDTO.toDomain())
+//            }
+            guard !task.isCancelled else { return nil }
 
             let endpoint = APIEndpoints.getMovies(with: requestDTO)
-            task.networkTask = self?.dataTransferService.request(
+            task.networkTask = self/*?*/.dataTransferService.request(
                 with: endpoint,
                 on: backgroundQueue
             ) { result in
                 switch result {
                 case .success(let responseDTO):
-                    self?.cache.save(response: responseDTO, for: requestDTO)
+//                    self?.cache.save(response: responseDTO, for: requestDTO)
                     completion(.success(responseDTO.toDomain()))
                 case .failure(let error):
                     completion(.failure(error))
                 }
             }
-        }
+//        }
         return task
     }
 }
